dd94512bdd23ccbd2d00c41e3666896eb3cf2018
jaspreetkaurrathod <168926898+jaspreetkaurrathod@users.noreply.github.com>
2024-06-14 22:29:03 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Jaspreet kaur rathod",
+  title: "Web Designer,Software tester & Content Creator",
+  email: "kaurjaspreeth02@gmail.com",
+  gitHub: "Jaspreet kaur rathod",
+  instagram: "",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
